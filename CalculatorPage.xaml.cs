df9e2316cb07565cfe9c771a3893eb9bb36e8702
Jacob Robinson <95jacob07@gmail.com>
2019-10-21 15:13:17 -0700

    storing values in their own variable now

--- CalculatorPage.xaml.cs
+++ CalculatorPage.xaml.cs
@@ -12,73 +12,160 @@ namespace Calculator
     public partial class CalculatorPage : Page
     {
         private bool displayingResult { get; set; }
+        private string firstNumber { get; set; } = "0";
+        private string secondNumber { get; set; } = "0";
+        private string chosenOpperator { get; set; } = "0";
+        private bool operatorWasChosen { get; set; } = false;
+        private int step { get; set; } = 1; // 1 == choosing first val,  2 == choosing second val
 
         public CalculatorPage()
         {
             InitializeComponent();
 
-            display.Text = "0";
-            equals.Click += Calculate;
+            topDisplay.Text = "";
+            bottomDisplay.Text = "0";
+
+            equals.Click += Equals;
             delete.Click += Delete;
+            clearAll.Click += ClearAll;
             clear.Click += Clear;
-            clear2.Click += Clear;
+            negativePositiveToggle.Click += NegativePositiveToggle;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var buttonValue = (e.Source as Button).Content.ToString();
-            char currentOperator = hasCalcOperator();
 
             if (isActionOperator(buttonValue))
                 return;
 
-            if ((displayingResult || display.Text == "0") && !isCalcOperator(buttonValue))
-            {
-                display.Text = buttonValue;
-                displayingResult = false;
-            }
-            else if (isCalcOperator(buttonValue) && currentOperator != '0')
-            {
-                int indexOfOperator = display.Text.IndexOf(currentOperator);
-                var displaySB = new StringBuilder(display.Text);
-                displaySB[indexOfOperator] = Convert.ToChar(buttonValue);
+            if (isCalcOperator(buttonValue))
+            { 
+                if (displayingResult)
+                {
+                    firstNumber = bottomDisplay.Text;
+                    bottomDisplay.Text = "0";
+                    secondNumber = "0";
+                    displayingResult = false;
+                }
 
-                display.Text = displaySB.ToString();
-            }
-            else
-                display.Text += buttonValue;
-        }
+                if (step == 1)
+                {
+                    chosenOpperator = buttonValue;
+                    operatorWasChosen = true;
 
-        private void Calculate(object sender, RoutedEventArgs e)
-        {
-            string[] values = display.Text.Split('+', '-', '×', '÷');
+                    topDisplay.Text = firstNumber + chosenOpperator;
+                    bottomDisplay.Text = "0";
+                    step++;
+                }
+                else
+                {
+                    firstNumber = Calculate().ToString();
+                    secondNumber = "0";
 
-            var firstNumber = float.Parse(values[0]);
-            var secondNumber = float.Parse(values[1]);
-            float answer;
+                    chosenOpperator = buttonValue;
+                    operatorWasChosen = true;
 
-            if (display.Text.Contains('+'))
-                answer = firstNumber + secondNumber;
-            else if (display.Text.Contains('-'))
-                answer = firstNumber - secondNumber;
-            else if (display.Text.Contains('×'))
-                answer = firstNumber * secondNumber;
-            else if (display.Text.Contains('÷') && secondNumber != 0)
-                answer = firstNumber / secondNumber;
-            else
-            {
-                display.Text = "Error";
+                    topDisplay.Text = firstNumber + chosenOpperator;
+                    bottomDisplay.Text = secondNumber;
+                }
+
                 return;
             }
 
-            display.Text = answer.ToString();
-            displayingResult = true;
+            string newDisplayNumber;
+            if (displayingResult)
+            {
+                newDisplayNumber = buttonValue;
+                displayingResult = false;
+            }
+            else if (step == 1)
+                newDisplayNumber = firstNumber = updateNumber(buttonValue);
+            else
+                newDisplayNumber = secondNumber = updateNumber(buttonValue);
+
+            bottomDisplay.Text = newDisplayNumber;            
         }
 
-        private void Delete(object sender, RoutedEventArgs e) =>
-            display.Text = display.Text.Length > 1 ? display.Text.Substring(0, display.Text.Length - 1) : "0";
+        private float Calculate()
+        {
+            var first = float.Parse(firstNumber);
+            var second = float.Parse(secondNumber);
+            float answer;
 
-        private void Clear(object sender, RoutedEventArgs e) => display.Text = "0";
+            if (chosenOpperator == "+")
+                answer = first + second;
+            else if (chosenOpperator == "-")
+                answer = first - second;
+            else if (chosenOpperator == "×")
+                answer = first * second;
+            else
+                answer = second != 0 ? first / second : 0;
+
+            return answer;
+        }
+
+        private void Equals(object sender, RoutedEventArgs e)
+        {
+            float answer = Calculate();
+
+            topDisplay.Text = firstNumber + chosenOpperator + secondNumber + "=";
+            bottomDisplay.Text = answer.ToString();
+            displayingResult = true;
+            operatorWasChosen = false;
+        }
+
+        private string updateNumber(string numberUpdate)
+        {
+            if (bottomDisplay.Text == "0")
+                return numberUpdate;
+
+            return bottomDisplay.Text + numberUpdate;
+        }
+
+        private void NegativePositiveToggle(object sender, RoutedEventArgs e)
+        {
+            string activeNumber = step == 1 ? firstNumber : secondNumber;
+            string newNumber = activeNumber[0] == '-' ? activeNumber.Substring(1) : "-" + activeNumber;
+
+            bottomDisplay.Text = newNumber;
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            string newValue = bottomDisplay.Text.Length > 1 ? bottomDisplay.Text.Substring(0, bottomDisplay.Text.Length - 1) : "0";
+
+            if (step == 1)
+                firstNumber = newValue;
+            else
+                secondNumber = newValue;
+
+            displayingResult = false;
+            bottomDisplay.Text = newValue;
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            bottomDisplay.Text = "0";
+            topDisplay.Text = "";
+
+            if (step == 1)
+                firstNumber = "0";
+            else
+                secondNumber = "0";
+
+            displayingResult = false;
+        }
+
+        private void ClearAll(object sender, RoutedEventArgs e)
+        {
+            firstNumber = "0";
+            secondNumber = "0";
+            topDisplay.Text = "";
+            bottomDisplay.Text = firstNumber;
+            displayingResult = false;
+            step = 1;
+        }
 
         private bool isActionOperator(string value)
         {
@@ -114,23 +201,6 @@ namespace Calculator
                 default:
                     return false;
             }
-        }
-
-        private char hasCalcOperator()
-        {
-            if (display.Text.Contains('+'))
-                return '+';
-
-            if (display.Text.Contains('-'))
-                return '-';
-
-            if (display.Text.Contains('×'))
-                return '×';
-
-            if (display.Text.Contains('÷'))
-                return '÷';
-
-            return '0';
         }
     }
 }
